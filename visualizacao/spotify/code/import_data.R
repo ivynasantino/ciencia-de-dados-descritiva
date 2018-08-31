f58bdd50d2126604807ef0f1b9c735ff52b043ea
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-08-31 11:45:54 -0300

    adiciona scripts para importar os dados do spotify

---
+++ visualizacao/spotify/code/import_data.R
@@ -0,0 +1,76 @@
+# install.packages("spotifyr")
+
+library(tidyverse)
+library(here)
+library(spotifyr)
+
+# Você precisará pegar um id e segredo para seu código aqui: https://developer.spotify.com/my-applications/#!/applications 
+# 
+chaves = read_csv(here::here("code/chaves-do-spotify.csv"), 
+                  col_types = "cc")
+
+Sys.setenv(SPOTIFY_CLIENT_ID = pull(chaves, client_id))
+Sys.setenv(SPOTIFY_CLIENT_SECRET = pull(chaves, client_secret))
+
+access_token <- get_spotify_access_token()
+
+#
+# PRECISA SER RODADO INTERATIVO, linha a linha: 
+#
+
+#lua <- get_artist_audio_features('luiz gonzaga')
+#lua %>% 
+#    write_csv(here("data/luiz_gonzaga.csv"))
+
+#lua = lua %>% 
+ #   mutate(artist = "Luiz Gonzaga")
+
+
+#jackson <- get_artist_audio_features('jackson do pandeiro')
+#jackson %>% 
+ #   write_csv(here("data/jackson.csv"))
+
+#jackson %>% 
+ #   mutate(artist = "Jackson do Pandeiro") %>% 
+  #  bind_rows(lua) %>% 
+   # write_csv(here("data/forro_do_gogó_ao_mocotó.csv"))
+
+
+#c1 <- get_artist_audio_features('Caetano Veloso') %>% 
+ #   mutate(artist = "Caetano Veloso")
+#c2 <- get_artist_audio_features('Gilberto Gil') %>% 
+ #   mutate(artist = "Gilberto Gil")
+#c3 <- get_artist_audio_features('Gal Costa') %>% 
+ #   mutate(artist = "Gal Costa")
+#c4 <- get_artist_audio_features('Maria Bethânia') %>% 
+ #   mutate(artist = "Maria Bethânia")
+#c5 <- get_artist_audio_features('Tom Zé') %>% 
+ #   mutate(artist = "Tom Zé")
+
+#bind_rows(c1, c2, c3, c4, c5) %>% 
+ #   write_csv(here("data/tropicalistas.csv"))
+
+#o_rei <- get_artist_audio_features('Roberto Carlos') %>% 
+ #   mutate(artist = "Roberto Carlos")
+#o_rei %>% 
+ #   write_csv(here("data/o_rei.csv"))
+
+tropicalistas = get_user_audio_features("ivynasantino") %>% 
+    filter(playlist_name == "Só as poderosas - tropicalismo")
+
+tropicalistas %>% 
+    write_csv(here::here("data/mulheres-tropicalismo.csv"))
+
+jovem_guarda = get_user_audio_features("ivynasantino") %>% 
+    filter(playlist_name == "Só as poderosas - Jovem guarda")
+
+jovem_guarda %>% 
+    write_csv(here::here("data/mulheres_jovem_guarda.csv"))
+
+bossa = get_user_audio_features("ivynasantino") %>% 
+    filter(playlist_name == "Só as poderosas - Bossa nova")
+
+bossa %>% 
+    write_csv(here::here("data/mulheres_bossa_nova.csv"))
+
+
